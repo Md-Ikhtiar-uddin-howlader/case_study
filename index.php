15467139223d2d5d75cf0d5ff9a39f1a34df1b3c
Md-Ikhtiar-uddin-howlader <sojib5304@gmail.com>
2022-04-18 00:17:09 +0800

    only for test

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <!-- <h1 class="headline">Welcome to IIUM cafe</h1> -->
     <marquee onmouseover="stop()"; onmouseout="start()" class="headline" ><h1>Welcome to IIUM cafe</h1></marquee>
     <h3 class ="heading">Please select your desired food</h3>
+    <h1>just test</h1>
     <?php
 class Product{
    public $name;
@@ -38,12 +39,17 @@ class Product{
 }
 
 $chickenBurger = new Product;
+$chickenBurgerCheese = new Product;
+$bbqChickenBurger = new Product;
+$beefBurger = new Product;
+$beefBurgerCheese = new Product;
 
 $chickenBurger->setValue("Chicken burger", "This is chicken burger with fried chicken", 13.5);
-$chickenBurgerCheese->setValue("Chicken burger with cheese", "This is chicken burger with fried chicken and cheese", 14.5);
+$chickenBurgerCheese->setValue("Chicken burger with cheese","This is chicken burger with fried chicken and cheese", 14.5);
 $bbqChickenBurger->setValue("BBQ Chicken burger with cheese", "This is BBQ chicken burger with bbq chicken petty and cheese", 17.0);
 $beefBurger->setValue("Beef burger", "This is beef burger with beef petty", 14);
 $beefBurgerCheese->setValue("Beef burger with Cheese", "This is beef burger with beef petty and cheese", 15);
+
 ?>
 </body>
 </html>
