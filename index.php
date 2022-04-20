3d6f4bf99d7ed4993fc27c1f85e5b19b3a74a141
Haris Zaki <103732219+hariszvki@users.noreply.github.com>
2022-04-20 13:46:03 +0800

    Revert "haris-invoice-popup"

--- index.php
+++ index.php
@@ -257,66 +257,7 @@ $cheeseBun->setValue("Cheese bun", "Small cheese bun", 1.5);
 </tbody>
  </table><br><br>
 
-
-<!-- Trigger/Open The Modal -->
-<center><button class="button" id="myBtn">SUBMIT FOR INVOICE</button></center>
-
-<!-- The Modal -->
-<div id="myModal" class="modal">
-
-  <!-- Modal content -->
-  <div class="modal-content">
-    <span class="close">&times;</span>
-
-    <table class="content-table">
-    <thead>
-      <tr>
-        <th>Amount</th>
-        <th>Ramadhan Discount (2%)</th>
-        <th>GST (6%)</th>
-        <th>Final Amount</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-      </tr>
-    </tbody>
-  </div>
-
-</div>
-
-<script>
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-</script>
-
+ <center><button class="button">SUBMIT FOR INVOICE</button></center>
 
 </body>
 </html>
